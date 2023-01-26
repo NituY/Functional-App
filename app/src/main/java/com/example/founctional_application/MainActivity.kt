f945a622528d720f2eed39a90aa572987a8f0f7b
NituY <nituyadav2803@gmail.com>
2023-01-26 18:02:57 +0530

    Initial Commit

--- app/src/main/java/com/example/founctional_application/MainActivity.kt
+++ app/src/main/java/com/example/founctional_application/MainActivity.kt
@@ -6,54 +6,43 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.telephony.TelephonyManager
+import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
-    //IMEI noumber
-//    lateinit var button: Button
-//    lateinit var textView: TextView
-//    private lateinit var IMEINumber: String
-//    private val REQUEST_CODE = 101
     // timestamp
     lateinit var calendar: Calendar
     lateinit var simpleDateFormat: SimpleDateFormat
     lateinit var date : String
-    lateinit var timeStamp : TextView
-    lateinit var TimeButton : Button
+    private lateinit var timeStamp : TextView
+    private lateinit var TimeButton : Button
 
     // location
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private  lateinit var locTv :TextView
+    private lateinit var locButton: Button
+    // internet
+    private  lateinit var cld: ConnectionLiveData
+    private lateinit var layout1: ConstraintLayout
+    private lateinit var layout2: ConstraintLayout
 
     @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        title = "KotlinApp"
-//        textView = findViewById(R.id.textView)
-//        button = findViewById(R.id.button)
-//        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as
-//                TelephonyManager
-//      //  IMEINumber = telephonyManager.deviceId
-//        textView.text = IMEINumber
-//        button.setOnClickListener {
-//
-//            if (ActivityCompat.checkSelfPermission(this@MainActivity,
-//                    Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(this@MainActivity,
-//                    arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_CODE)
-//                return@setOnClickListener
-//            }
-//
-//        }
+
         timeStamp = findViewById(R.id.timeTv)
         TimeButton = findViewById(R.id.TimeBtn)
         calendar = Calendar.getInstance()
@@ -62,49 +51,86 @@ class MainActivity : AppCompatActivity() {
         TimeButton.setOnClickListener {
             timeStamp.text = date
         }
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        //location
+        locTv = findViewById(R.id.loctv)
+        locButton = findViewById(R.id.locationBtn)
+        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(this)
 
-        findViewById<Button>(R.id.locationBtn).setOnClickListener {
+        locButton.setOnClickListener {
             fetchLocation()
+
         }
+        // internet
+        checkNetworkConnection()
+        layout1 = findViewById(R.id.layout1)
+        layout2 = findViewById(R.id.layout2)
+
+
+
 
 
     }
 
-    private fun fetchLocation() {
-        val task = fusedLocationProviderClient.lastLocation
+    private fun checkNetworkConnection() {
+        cld= ConnectionLiveData(application)
+        cld.observe(this, { isConnected->
+            if (isConnected){
+                layout1.visibility = View.VISIBLE
+                layout2.visibility = View.GONE
 
-        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
-        !=PackageManager.PERMISSION_GRANTED && ActivityCompat.
-            checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
-            return
-        }
-        task.addOnSuccessListener {
-            if(it != null){
-                Toast.makeText(applicationContext,"${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
+            }else{
+                layout1.visibility = View.GONE
+                layout2.visibility = View.VISIBLE
 
             }
-        }
-
+        })
     }
 
 
+    //location
+    private fun fetchLocation() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
+        }else{
+            getLocattions()
+        }
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
-//                                            grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        when (requestCode) {
-//            REQUEST_CODE -> {
-//                if (grantResults.isNotEmpty() && grantResults[0] ==
-//                    PackageManager.PERMISSION_GRANTED) {
-//                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show()
-//                } else {
-//                    Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        }
-//    }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocattions() {
+        fusedLocationProviderClient.lastLocation?.addOnSuccessListener {
+            if (it == null){
+                Toast.makeText(this,"Sorry Can't get Location", Toast.LENGTH_SHORT).show()
+            }else it.apply {
+                val lattitudde = it.latitude
+                val  long= it.longitude
+                locTv.text = "Latitude :$lattitudde, Longitude:$long"
+            }
+
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode ==101){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    getLocattions()
+                }
+                else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 }
 
